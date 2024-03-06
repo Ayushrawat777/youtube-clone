ea3e4e0380d8c0ab232c65e4d36a2a18d95d02a9
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-03-06 16:05:02 +0530

    first commit3

--- src/push.jsx
+++ src/push.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const push = () => {
   return (
-    <div>push</div>
+    <div>pusherwerw</div>
   )
 }
 
---
+++ src/push2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const push2 = () => {
+  return (
+    <div>push2</div>
+  )
+}
+
+export default push2
